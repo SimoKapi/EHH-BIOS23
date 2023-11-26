84fb79f99657fb7720e93195ebe3401a27c2d8c8
SimoKapi <simon.kapicka@gmail.com>
2023-11-26 12:07:28 +0100

    Final touches

--- frontend/login.js
+++
@@ -1,7 +0,0 @@
- function submit_data(){
-    console.log({
-        "Provider": document.getElementById("provider_data").value,
-        "Username": document.getElementById("username_data").value,
-        "Password": document.getElementById("password_data").value
-    })
- }
